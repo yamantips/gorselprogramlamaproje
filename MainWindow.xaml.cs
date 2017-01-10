0ef54f104ee113335b4565e9b78d97bb00737226
yamantips <harunyaman01@gmail.com>
2017-01-10 13:49:22 +0300

    kodlama
    mysql baglantıları yapıldı paneller açıldı

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,8 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
 
-namespace WpfApplication2
+namespace harun
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +27,36 @@ namespace WpfApplication2
         {
             InitializeComponent();
         }
-    }
-}
+
+
+        MySqlConnection baglanti = new MySqlConnection("Server=localhost;Database=tblmezar;Uid=root;Pwd='';");
+
+        void olustur()
+        {
+
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT UID FROM tblmezar as m left join tbluye as u on u.UMID=m.MID ORDER BY m.MID", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            panel.Children.Clear();
+            panel2.Children.Clear();
+            panel3.Children.Clear();
+            panel4.Children.Clear();
+            panel5.Children.Clear();
+
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Name = "btn" + (i + 1).ToString();
+                btn.Tag = i + 1;
+                if (dt.Rows[i]["UID"].ToString() != "")
+                {
+                    btn.Background = new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    btn.Background = new SolidColorBrush(Colors.Green);
+
+                }
+            }
